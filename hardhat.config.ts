b9b6028dff2bca8885d5d464f03c2ac6b4693d79
imollov <ivomollov@gmail.com>
2024-11-19 14:44:29 +0200

    chore: setup hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,31 @@
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import * as dotenv from 'dotenv'
+
+// Load environment variables
+dotenv.config()
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.20',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    optimism: {
+      url: process.env.OP_MAINNET_URL || '',
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+}
+
+export default config
