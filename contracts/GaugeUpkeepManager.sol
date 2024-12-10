30c7ce7e4ecec4c3a08919d7436d49008c154617
Ivelin Mollov <ivelin@hack.bg>
2024-12-10 13:04:29 +0200

    Owner functions (#7)
    * feat: owner functions
    
    * withdraw contract link balance
    * transfer upkeep admin rights
    * set new upkeep gas limit and fund amount
    
    * Docs (#9)
    
    * docs: automation flow
    
    * add natspec to automation interface functions
    * refactor comment style
    
    * docs: readme
    
    * Secure perform upkeep (#10)
    
    * feat: secure perform upkeep
    
    * Split contract (#11)
    
    * refactor: decouple cron upkeep factory
    
    * Setup CI (#14)
    
    * ci: tests
    
    * chore: setup formatter
    
    * chore: run formatter
    
    * ci: lint
    
    * test: trusted forwarder

--- contracts/GaugeUpkeepManager.sol
+++ contracts/GaugeUpkeepManager.sol
@@ -1,74 +1,87 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ILogAutomation, Log} from "@chainlink/contracts/src/v0.8/automation/interfaces/ILogAutomation.sol";
 import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
 import {IKeeperRegistryMaster} from "@chainlink/contracts/src/v0.8/automation/interfaces/v2_1/IKeeperRegistryMaster.sol";
 import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
-import {CronUpkeep} from "@chainlink/contracts/src/v0.8/automation/upkeeps/CronUpkeep.sol";
-import {Spec, Cron} from "@chainlink/contracts/src/v0.8/automation/libraries/external/Cron.sol";
 import {IAutomationRegistrar, RegistrationParams} from "./interfaces/IAutomationRegistrar.sol";
 import {IGaugeUpkeepManager} from "./interfaces/IGaugeUpkeepManager.sol";
+import {ICronUpkeepFactory} from "./interfaces/ICronUpkeepFactory.sol";
 
-contract GaugeUpkeepManager is IGaugeUpkeepManager, ILogAutomation, AutomationCompatibleInterface {
-    // @inheritdoc IGaugeUpkeepManager
-    address public override immutable linkToken;
-    // @inheritdoc IGaugeUpkeepManager 
-    address public override immutable keeperRegistry;
-    // @inheritdoc IGaugeUpkeepManager
-    address public override immutable automationRegistrar;
-    // @inheritdoc IGaugeUpkeepManager
-    address public override immutable automationCronDelegate;
-    // @inheritdoc IGaugeUpkeepManager
-    address public override immutable voter;
-    
-    // @inheritdoc IGaugeUpkeepManager
-    uint96 public override upkeepFundAmount;
-    // @inheritdoc IGaugeUpkeepManager
-    uint32 public override upkeepGasLimit;
+contract GaugeUpkeepManager is IGaugeUpkeepManager, ILogAutomation, AutomationCompatibleInterface, Ownable {
+    /// @inheritdoc IGaugeUpkeepManager
+    address public immutable override linkToken;
+    /// @inheritdoc IGaugeUpkeepManager
+    address public immutable override keeperRegistry;
+    /// @inheritdoc IGaugeUpkeepManager
+    address public immutable override automationRegistrar;
+    /// @inheritdoc IGaugeUpkeepManager
+    address public immutable override cronUpkeepFactory;
+    /// @inheritdoc IGaugeUpkeepManager
+    address public immutable override voter;
 
-    // @inheritdoc IGaugeUpkeepManager
+    /// @inheritdoc IGaugeUpkeepManager
+    address public override trustedForwarder;
+    /// @inheritdoc IGaugeUpkeepManager
+    uint96 public override newUpkeepFundAmount;
+    /// @inheritdoc IGaugeUpkeepManager
+    uint32 public override newUpkeepGasLimit;
+
+    /// @inheritdoc IGaugeUpkeepManager
     mapping(address => uint256) public override gaugeUpkeepId;
-    // @inheritdoc IGaugeUpkeepManager
+    /// @inheritdoc IGaugeUpkeepManager
     address[] public override cancelledGaugeUpkeeps;
-    // @inheritdoc IGaugeUpkeepManager
+    /// @inheritdoc IGaugeUpkeepManager
     mapping(address => uint256) public override cancelledUpkeepBlockNumber;
 
     uint8 private constant CONDITIONAL_TRIGGER_TYPE = 0;
-    uint256 private constant CRON_UPKEEP_MAX_JOBS = 1;
     uint256 private constant CANCELLATION_DELAY_BLOCKS = 100;
     string private constant UPKEEP_NAME = "cron upkeep";
     string private constant CRON_EXPRESSION = "0 0 * * 3";
     string private constant DISTRIBUTE_FUNCTION = "distribute(address)";
-    
-    bytes32 private constant GAUGE_CREATED_SIGNATURE = 0xef9f7d1ffff3b249c6b9bf2528499e935f7d96bb6d6ec4e7da504d1d3c6279e1;
-    bytes32 private constant GAUGE_KILLED_SIGNATURE = 0x04a5d3f5d80d22d9345acc80618f4a4e7e663cf9e1aed23b57d975acec002ba7;
-    bytes32 private constant GAUGE_REVIVED_SIGNATURE = 0xed18e9faa3dccfd8aa45f69c4de40546b2ca9cccc4538a2323531656516db1aa;
 
-    enum PerformAction { REGISTER_UPKEEP, CANCEL_UPKEEP, WITHDRAW_UPKEEP_BALANCE }
+    bytes32 private constant GAUGE_CREATED_SIGNATURE =
+        0xef9f7d1ffff3b249c6b9bf2528499e935f7d96bb6d6ec4e7da504d1d3c6279e1;
+    bytes32 private constant GAUGE_KILLED_SIGNATURE =
+        0x04a5d3f5d80d22d9345acc80618f4a4e7e663cf9e1aed23b57d975acec002ba7;
+    bytes32 private constant GAUGE_REVIVED_SIGNATURE =
+        0xed18e9faa3dccfd8aa45f69c4de40546b2ca9cccc4538a2323531656516db1aa;
+
+    enum PerformAction {
+        REGISTER_UPKEEP,
+        CANCEL_UPKEEP,
+        WITHDRAW_UPKEEP_BALANCE
+    }
 
     error InvalidPerformAction();
     error AutoApproveDisabled();
+    error UnauthorizedSender();
 
     constructor(
         address _linkToken,
         address _keeperRegistry,
         address _automationRegistrar,
-        address _automationCronDelegate,
+        address _cronUpkeepFactory,
         address _voter,
-        uint96 _upkeepFundAmount,
-        uint32 _upkeepGasLimit
+        uint96 _newUpkeepFundAmount,
+        uint32 _newUpkeepGasLimit
     ) {
         linkToken = _linkToken;
         keeperRegistry = _keeperRegistry;
         automationRegistrar = _automationRegistrar;
-        automationCronDelegate = _automationCronDelegate;
+        cronUpkeepFactory = _cronUpkeepFactory;
         voter = _voter;
-        upkeepFundAmount = _upkeepFundAmount;
-        upkeepGasLimit = _upkeepGasLimit;
+        newUpkeepFundAmount = _newUpkeepFundAmount;
+        newUpkeepGasLimit = _newUpkeepGasLimit;
     }
 
-    // @inheritdoc ILogAutomation
+    /// @inheritdoc ILogAutomation
+    /// @notice Called by the automation DON when a new log is emitted by the target contract
+    /// @dev This function is called by the automation DON to check if any action is needed
+    /// @return upkeepNeeded True if any action is needed according to the log
+    /// @return performData Encoded action and data passed to performUpkeep if upkeepNeeded is true
     function checkLog(
         Log calldata log,
         bytes memory
@@ -93,7 +106,12 @@ contract GaugeUpkeepManager is IGaugeUpkeepManager, ILogAutomation, AutomationCo
         }
     }
 
-    // @inheritdoc AutomationCompatibleInterface
+    /// @inheritdoc AutomationCompatibleInterface
+    /// @notice Check if any cancelled gauge upkeeps are ready to be withdrawn
+    /// @dev This function is called by the automation DON to check if any upkeeps are needed
+    /// @dev Upkeep balance can be withdrawn after a delay of certain blocks
+    /// @return upkeepNeeded True if any cancelled gauge upkeeps are ready to be withdrawn
+    /// @return performData Encoded action and data passed to performUpkeep if upkeepNeeded is true
     function checkUpkeep(bytes calldata) external view override returns (bool upkeepNeeded, bytes memory performData) {
         for (uint256 i = 0; i < cancelledGaugeUpkeeps.length; i++) {
             address gauge = cancelledGaugeUpkeeps[i];
@@ -103,9 +121,15 @@ contract GaugeUpkeepManager is IGaugeUpkeepManager, ILogAutomation, AutomationCo
         }
     }
 
-    // @inheritdoc ILogAutomation
-    function performUpkeep(bytes calldata performData) external override(ILogAutomation, AutomationCompatibleInterface) {
-        // todo: check if the sender is trusted forwarder
+    /// @inheritdoc AutomationCompatibleInterface
+    /// @notice Perform the upkeep action according to the performData passed from checkUpkeep/checkLog
+    /// @dev This function is called by the automation network to perform the upkeep action
+    function performUpkeep(
+        bytes calldata performData
+    ) external override(ILogAutomation, AutomationCompatibleInterface) {
+        if (msg.sender != trustedForwarder) {
+            revert UnauthorizedSender();
+        }
         (PerformAction action, address gauge) = abi.decode(performData, (PerformAction, address));
         if (action == PerformAction.REGISTER_UPKEEP) {
             _registerGaugeUpkeep(gauge);
@@ -120,28 +144,23 @@ contract GaugeUpkeepManager is IGaugeUpkeepManager, ILogAutomation, AutomationCo
     }
 
     function _registerGaugeUpkeep(address gauge) internal returns (uint256 upkeepId) {
-        bytes memory job = _encodeCronJob(
+        bytes memory job = ICronUpkeepFactory(cronUpkeepFactory).encodeCronJob(
             voter,
             abi.encodeWithSignature(DISTRIBUTE_FUNCTION, gauge),
             CRON_EXPRESSION
         );
-        CronUpkeep cronUpkeep = new CronUpkeep(
-            address(this),
-            automationCronDelegate,
-            CRON_UPKEEP_MAX_JOBS,
-            job
-        );
+        address cronUpkeep = ICronUpkeepFactory(cronUpkeepFactory).newCronUpkeepWithJob(job);
         RegistrationParams memory params = RegistrationParams({
             name: UPKEEP_NAME,
             encryptedEmail: "",
             upkeepContract: address(cronUpkeep),
-            gasLimit: upkeepGasLimit,
+            gasLimit: newUpkeepGasLimit,
             adminAddress: address(this),
             triggerType: CONDITIONAL_TRIGGER_TYPE,
             checkData: "",
             triggerConfig: "",
             offchainConfig: "",
-            amount: upkeepFundAmount
+            amount: newUpkeepFundAmount
         });
         upkeepId = _registerUpkeep(params);
         gaugeUpkeepId[gauge] = upkeepId;
@@ -186,25 +205,36 @@ contract GaugeUpkeepManager is IGaugeUpkeepManager, ILogAutomation, AutomationCo
         delete gaugeUpkeepId[gauge];
     }
 
-    function _encodeCronJob(
-        address target,
-        bytes memory handler,
-        string memory cronString
-    ) internal pure returns (bytes memory) {
-        Spec memory spec = Cron.toSpec(cronString);
-        return abi.encode(target, handler, spec);
-    }
-
     function _extractGaugeFromCreatedLog(Log memory log) internal pure returns (address gauge) {
-        (,,,gauge,) = abi.decode(log.data, (address, address, address, address, address));
+        (, , , gauge, ) = abi.decode(log.data, (address, address, address, address, address));
     }
 
     function _bytes32ToAddress(bytes32 _address) internal pure returns (address) {
         return address(uint160(uint256(_address)));
     }
 
-    // todo: withdraw link function
-    // todo: transfer upkeep function
-    // todo: set gas limit function
-    // todo: set fund amount function
+    /// @inheritdoc IGaugeUpkeepManager
+    function withdrawLinkBalance() external override onlyOwner {
+        LinkTokenInterface(linkToken).transfer(owner(), LinkTokenInterface(linkToken).balanceOf(address(this)));
+    }
+
+    /// @inheritdoc IGaugeUpkeepManager
+    function transferUpkeepAdmin(uint256 upkeepId, address newAdmin) external override onlyOwner {
+        IKeeperRegistryMaster(keeperRegistry).transferUpkeepAdmin(upkeepId, newAdmin);
+    }
+
+    /// @inheritdoc IGaugeUpkeepManager
+    function setNewUpkeepGasLimit(uint32 _newUpkeepGasLimit) external override onlyOwner {
+        newUpkeepGasLimit = _newUpkeepGasLimit;
+    }
+
+    /// @inheritdoc IGaugeUpkeepManager
+    function setNewUpkeepFundAmount(uint96 _newUpkeepFundAmount) external override onlyOwner {
+        newUpkeepFundAmount = _newUpkeepFundAmount;
+    }
+
+    /// @inheritdoc IGaugeUpkeepManager
+    function setTrustedForwarder(address _trustedForwarder) external override onlyOwner {
+        trustedForwarder = _trustedForwarder;
+    }
 }
--- contracts/interfaces/IGaugeUpkeepManager.sol
+++ contracts/interfaces/IGaugeUpkeepManager.sol
@@ -6,33 +6,56 @@ interface IGaugeUpkeepManager {
     event GaugeUpkeepCancelled(address gauge, uint256 upkeepId);
     event GaugeUpkeepWithdrawn(address gauge, uint256 upkeepId);
 
-    // @notice LINK token address
+    /// @notice LINK token address
     function linkToken() external view returns (address);
 
-    // @notice Keeper registry address
+    /// @notice Keeper registry address
     function keeperRegistry() external view returns (address);
 
-    // @notice Automation registrar address
+    /// @notice Automation registrar address
     function automationRegistrar() external view returns (address);
 
-    // @notice Automation cron delegate address
-    function automationCronDelegate() external view returns (address);
+    /// @notice Automation cron delegate address
+    function cronUpkeepFactory() external view returns (address);
 
-    // @notice Voter address
+    /// @notice Voter address
     function voter() external view returns (address);
 
-    // @notice Amount of LINK to transfer to upkeep on registration
-    function upkeepFundAmount() external view returns (uint96);
+    /// @notice Automation trusted forwarder address
+    function trustedForwarder() external view returns (address);
 
-    // @notice Gas limit for new upkeeps
-    function upkeepGasLimit() external view returns (uint32);
+    /// @notice Amount of LINK to transfer to upkeep on registration
+    function newUpkeepFundAmount() external view returns (uint96);
 
-    // @notice Upkeep ID for a gauge
+    /// @notice Gas limit for new upkeeps
+    function newUpkeepGasLimit() external view returns (uint32);
+
+    /// @notice Upkeep ID for a gauge
     function gaugeUpkeepId(address gauge) external view returns (uint256);
 
-    // @notice Gauge addresses of cancelled upkeeps
+    /// @notice Gauge addresses of cancelled upkeeps
     function cancelledGaugeUpkeeps(uint256 index) external view returns (address);
 
-    // @notice Block number when the upkeep was cancelled
+    /// @notice Block number when the upkeep was cancelled
     function cancelledUpkeepBlockNumber(address gauge) external view returns (uint256);
+
+    /// @notice Transfer contract LINK balance to owner
+    function withdrawLinkBalance() external;
+
+    /// @notice Transfer gauge upkeep admin rights to a new address
+    /// @param upkeepId Upkeep ID
+    /// @param newAdmin New admin address
+    function transferUpkeepAdmin(uint256 upkeepId, address newAdmin) external;
+
+    /// @notice Update the gas limit for new gauge upkeeps
+    /// @param newUpkeepGasLimit New upkeep gas limit
+    function setNewUpkeepGasLimit(uint32 newUpkeepGasLimit) external;
+
+    /// @notice Update the LINK amount to transfer to new gauge upkeeps
+    /// @param newUpkeepFundAmount New upkeep fund amount
+    function setNewUpkeepFundAmount(uint96 newUpkeepFundAmount) external;
+
+    /// @notice Set the automation trusted forwarder address
+    /// @param trustedForwarder Upkeep trusted forwarder address
+    function setTrustedForwarder(address trustedForwarder) external;
 }
