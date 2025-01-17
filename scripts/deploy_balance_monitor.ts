64ad37832681dbd91f26de6bb9fc412c17604920
imollov <ivomollov@gmail.com>
2025-01-17 12:14:59 +0200

    fix: balance monitor deploy script

--- scripts/deploy_balance_monitor.ts
+++ scripts/deploy_balance_monitor.ts
@@ -11,20 +11,21 @@ import { verifyContract } from './utils'
 // Load environment variables
 dotenv.config()
 
-const GAUGE_UPKEEP_MANAGER_ADDRESS = process.env.GAUGE_UPKEEP_MANAGER_ADDRESS
+const LINK_TOKEN_ADDRESS = process.env.LINK_TOKEN_ADDRESS
+const KEEPER_REGISTRY_ADDRESS = process.env.KEEPER_REGISTRY_ADDRESS
 const MAX_BATCH_SIZE = process.env.MAX_BATCH_SIZE
 const MIN_PERCENTAGE = process.env.MIN_PERCENTAGE
 const TARGET_PERCENTAGE = process.env.TARGET_PERCENTAGE
 const MAX_TOP_UP_AMOUNT = process.env.MAX_TOP_UP_AMOUNT
+const MAX_ITERATIONS = process.env.MAX_ITERATIONS
 
-assert.ok(
-  GAUGE_UPKEEP_MANAGER_ADDRESS,
-  'GAUGE_UPKEEP_MANAGER_ADDRESS is required',
-)
+assert.ok(LINK_TOKEN_ADDRESS, 'LINK_TOKEN_ADDRESS is required')
+assert.ok(KEEPER_REGISTRY_ADDRESS, 'KEEPER_REGISTRY_ADDRESS is required')
 assert.ok(MAX_BATCH_SIZE, 'MAX_BATCH_SIZE is required')
 assert.ok(MIN_PERCENTAGE, 'MIN_PERCENTAGE is required')
 assert.ok(TARGET_PERCENTAGE, 'TARGET_PERCENTAGE is required')
 assert.ok(MAX_TOP_UP_AMOUNT, 'MAX_TOP_UP_AMOUNT is required')
+assert.ok(MAX_ITERATIONS, 'MAX_ITERATIONS is required')
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -39,12 +40,14 @@ async function main() {
     'UpkeepBalanceMonitor',
   )
   const upkeepBalanceMonitor = await UpkeepBalanceMonitor.deploy(
-    GAUGE_UPKEEP_MANAGER_ADDRESS!,
+    LINK_TOKEN_ADDRESS!,
+    KEEPER_REGISTRY_ADDRESS!,
     {
       maxBatchSize: MAX_BATCH_SIZE!,
       minPercentage: MIN_PERCENTAGE!,
       targetPercentage: TARGET_PERCENTAGE!,
       maxTopUpAmount: MAX_TOP_UP_AMOUNT!,
+      maxIterations: MAX_ITERATIONS!,
     },
   )
   await upkeepBalanceMonitor.deployed()
@@ -52,12 +55,14 @@ async function main() {
 
   // Verify UpkeepBalanceMonitor contract
   await verifyContract(upkeepBalanceMonitor.address, [
-    GAUGE_UPKEEP_MANAGER_ADDRESS!,
+    LINK_TOKEN_ADDRESS!,
+    KEEPER_REGISTRY_ADDRESS!,
     {
       maxBatchSize: MAX_BATCH_SIZE!,
       minPercentage: MIN_PERCENTAGE!,
       targetPercentage: TARGET_PERCENTAGE!,
       maxTopUpAmount: MAX_TOP_UP_AMOUNT!,
+      maxIterations: MAX_ITERATIONS!,
     },
   ])
 }
